92ae24fde7c6a0070fdaa2d25d54a2e18fc38fdd
Franky <frankyaquino101@gmail.com>
2019-05-11 20:14:15 -0400

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+import GameScene from './GameScene'
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: GameScene
+};
+
+export { config }
